732c1560c578f3538ffc9d83acbbc6759360b3c1
Fatima Bayramova <165319177+fiovba@users.noreply.github.com>
2025-06-17 23:02:53 +0400

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -10,7 +10,7 @@ import React, { useState } from 'react'
     <>
       <section className="dark:bg-gray-800 dark:text-gray-100">
             <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
-                <h1 className="text-4xl font-bold leading-none sm:text-5xl">Welcome to CountryApp Website
+                <h1 className="text-4xl font-bold text-purple-500 leading-none sm:text-5xl">Welcome to CountryApp Website
                 </h1>
                 <p className="px-8 mt-8 mb-12 text-lg">You can find data of any country in this website.</p>
                 <div className="flex flex-wrap justify-center">
